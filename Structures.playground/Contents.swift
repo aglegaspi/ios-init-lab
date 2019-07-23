7e9886572c98354c1b13db9ea2a104d30bff6f4b
aglegaspi <lex83nyc@gmail.com>
2019-07-23 18:47:27 -0400

    completed pg5

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -212,3 +212,46 @@ print(mile.feet)
 
 var secondDistance = Distance(meters: 192.934)
 print(secondDistance.feet)
+
+
+
+
+// PG.5
+
+//Exercise - Methods
+
+//A Book struct has been created for you below. Add an instance method on Book called description that will print out facts about the book. Then create an instance of Book and call this method on that instance.
+
+struct Book5 {
+    var title: String
+    var author: String
+    var pages: Int
+    var price: Double
+    
+    func description() {
+        print("\(title) by \(author) is \(pages) long and is $\(price)")
+    }
+}
+
+var bookInfo = Book5(title: "Swifty", author: "Rick Sanchez", pages: 1, price: 9.99)
+bookInfo.description()
+
+//A Post struct has been created for you below, representing a generic social media post. Add a mutating method on Post called like that will increment likes by one. Then create an instance of Post and call like() on it. Print out the likes property before and after calling the method to see whether or not the value was incremented.
+
+struct Post5 {
+    var message: String
+    var likes: Int
+    var numberOfComments: Int
+    
+    mutating func like() {
+        likes += 1
+    }
+}
+
+var myPost5 = Post5(message: "yo!", likes: 3, numberOfComments: 3)
+myPost5.likes
+myPost5.like()
+myPost5.likes
+
+
+
