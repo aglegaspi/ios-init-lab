c26ab73a54bbd32627d20ed01637b137b0e295f9
aglegaspi <lex83nyc@gmail.com>
2019-07-23 19:20:06 -0400

    completed pg 6

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -254,4 +254,44 @@ myPost5.like()
 myPost5.likes
 
 
+// PG. 6
+
+//App Exercise - Workout Functions
+
+//These exercises reinforce Swift concepts in the context of a fitness tracking app.
+
+//A RunningWorkout struct has been created for you below. Add a method on RunningWorkout called postWorkoutStats that prints out the details of the run. Then create an instance of RunningWorkout and call postWorkoutStats().
+
+struct RunningWorkout6 {
+    var distance: Double
+    var time: Double
+    var elevation: Double
+    
+    func postWorkoutStats() {
+        print("The distance you traveled was \(distance) meters. It took you \(time) minutes. With an elevation of \(elevation) meters")
+    }
+}
+
+var yourRunningResults = RunningWorkout6(distance: 20.33, time: 10.5, elevation: 10.8)
+yourRunningResults.postWorkoutStats()
+
+//A Steps struct has been created for you below, representing the day's step-tracking data. It has the goal number of steps for the day and the number of steps taken so far. Create a method on Steps called takeStep that increments the value of steps by one. Then create an instance of Steps and call takeStep(). Print the value of the instance's steps property before and after the method call.
+
+
+struct Steps {
+    
+    var steps: Int
+    var goal: Int
+    
+    mutating func takeStep() {
+        self.steps += 1
+    }
+
+}
+
+var tookAStep = Steps(steps: 5, goal: 10)
+tookAStep.steps
+tookAStep.takeStep()
+tookAStep.steps
+
 
